f71d38c250f597e67b72f52decdbaeafcd0ea938
Hidetaka Okamoto <hideokamoto@stripe.com>
2023-12-21 14:49:45 +0900

    test

--- api/index.ts
+++ api/index.ts
@@ -9,7 +9,7 @@ const app = new Hono().basePath('/api')
 
 app.get('/test', (c) => c.json({ message: 'Hello Hono!' }))
 app.get('/*', async c => {
-  const path = c.req.path
+  const path = c.req.path.replace(/^\/api/, '')
   console.log({path,d: `https://github.com/hideokamoto/practice-vercel-json${path}`})
   const data = await fetch(`https://github.com/hideokamoto/practice-vercel-json${path}`)
   return data
