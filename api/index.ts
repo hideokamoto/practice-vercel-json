0a8781209b1f058f782b04b09835ee7af63829fe
Hidetaka Okamoto <hideokamoto@stripe.com>
2023-12-21 15:57:25 +0900

    stripe.dev redirect test

--- api/index.ts
+++ api/index.ts
@@ -6,33 +6,13 @@ export const config = {
 }
 
 const app = new Hono()
-const apiApp = new Hono()
 const elementsApp = new Hono()
 
 elementsApp.get('/*', c => {
   const path = c.req.path.replace(/^\/elements-examples/, '')
-  console.log([
-    path,
-    c.req.path.replace(/^\/elements-examples/, ''),
-    c.req.path,
-    `https://stripe.dev/elements-examples${path}`
-  ])
   return fetch(`https://stripe.dev/elements-examples${path}`)
 })
 
-apiApp.get('/test', (c) => c.json({ message: 'Hello Hono!!!' }))
-app.route('/api', apiApp)
 app.route('/elements-examples', elementsApp)
-app.get('/*', async c => {
-  return c.html(`
-  <!DOCTYPE html>
-  <html>
-      <body>
-          <h1>Hell hono on Vercel</h1>
-          <p>We're in the <code>${c.req.path}</code></p>
-      </body>
-  </html>
-  `)
-})
 
 export default handle(app)
